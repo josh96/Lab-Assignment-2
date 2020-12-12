4c4c8ead54515537326fdd4b9a428014db9ec9bf
Josh Paredez <joshparedez96@Joshs-Air.attlocal.net>
2020-12-12 13:16:30 -0800

    fix rounding

--- stamp_program.py
+++ stamp_program.py
@@ -9,18 +9,23 @@ output the result to the user
 end
 """
 
+import math
 
+# input sheet
 def calculate(sheet):
+  # step 1
   answer = sheet / 5
+  # step 2
   rounded = round(answer, 1)
   print("sheet is : ", sheet)
   print("The answer is: ", answer)
-  print("rounded is: ", rounded)
+  print("rounded is: ", math. ceil(rounded))
   print("=================================")
+  # output: number of stamps needed
   return rounded
 
 
 output = calculate(16)
 
 
-print("the return statement is: ", output)
+print("the number of stamps needed is: ", output)
